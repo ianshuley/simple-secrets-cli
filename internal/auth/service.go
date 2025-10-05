02e2f1b47799bd5a2cffad044e4f41ac50f90f78
COMMAND_EXECUTED_BY_GIT <ianshuley@gmail.com>
2025-10-05 17:57:01 -0400

    chore: Remove unused package comments from multiple files

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -1,5 +1,4 @@
-/*package auth
-
+/*
 Copyright © 2025 Ian Shuley
 
 Licensed under the Apache License, Version 2.0 (the "License");
--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -1,5 +1,4 @@
-/*package errors
-
+/*
 Copyright © 2025 Ian Shuley
 
 Licensed under the Apache License, Version 2.0 (the "License");
