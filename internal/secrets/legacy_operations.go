972ad180e82347b39cabd26fb558d4f8f7d104fe
COMMAND_EXECUTED_BY_GIT <ianshuley@gmail.com>
2025-10-05 16:09:17 -0400

    fix(license): Update copyright notice in legacy_operations.go

--- internal/secrets/legacy_operations.go
+++ internal/secrets/legacy_operations.go
@@ -1,7 +1,5 @@
 /*
-Copyright © 2025 Ian Sh	\"simple-secrets/pkg/crypto\"
-
-	\"simple-secrets/internal/rotation\"ey
+Copyright © 2025 Ian Shuley
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
